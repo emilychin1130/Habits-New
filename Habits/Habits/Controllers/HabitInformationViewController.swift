d736762f3565f8e46beeed039b6cdd6b5362ace0
emilychin1130 <emilychin1130@gmail.com>
2017-07-17 11:39:35 -0700

    added everything

---
+++ Habits/Habits/Controllers/HabitInformationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HabitInformationViewController.swift
+//  Habits
+//
+//  Created by Emily Chin on 7/17/17.
+//  Copyright © 2017 Emily Chin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HabitInformationViewController: UIViewController {
+    @IBOutlet weak var habitNameTextField: UITextField!
+
+}
---
+++ Habits/Habits/Controllers/ListHabitsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListHabitsViewController.swift
+//  Habits
+//
+//  Created by Emily Chin on 7/17/17.
+//  Copyright © 2017 Emily Chin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListHabitsViewController: UIViewController {
+    @IBAction func unwindToListHabitsViewController(_ segue: UIStoryboardSegue) {
+        
+        // for now, simply defining the method is sufficient.
+        // we'll add code later
+        
+    }
+}
