b363abfb025d4192c5238e595bc442ffedfc3093
emilychin1130 <emilychin1130@gmail.com>
2017-07-20 11:03:52 -0700

    added parts of coredata

--- Habits/Habits/Controllers/HabitInformationViewController.swift
+++ Habits/Habits/Controllers/HabitInformationViewController.swift
@@ -11,4 +11,30 @@ import UIKit
 
 class HabitInformationViewController: UIViewController {
     @IBOutlet weak var habitNameTextField: UITextField!
+    
+    var habit: Habit?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            let habit = self.habit ?? CoreDataHelper.newHabit()
+            habit.habit = habitNameTextField.text ?? ""
+         //   habit.days =
+            CoreDataHelper.saveHabit()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let habit = habit {
+            habitNameTextField.text = habit.habit
+//            noteContentTextView.text = note.content
+        } else {
+            habitNameTextField.text = ""
+//            noteContentTextView.text = ""
+        }
+    }
 }
--- Habits/Habits/Controllers/ListHabitsViewController.swift
+++ Habits/Habits/Controllers/ListHabitsViewController.swift
@@ -12,14 +12,14 @@ import UIKit
 class ListHabitsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBAction func unwindToListHabitsViewController(_ segue: UIStoryboardSegue) {
 
-        // add code later
+        self.habits = CoreDataHelper.retrieveHabits()
         
     }
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        habit = CoreDataHelper.retrieveHabits()
+        habits = CoreDataHelper.retrieveHabits()
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -29,37 +29,62 @@ class ListHabitsViewController: UIViewController, UITableViewDataSource, UITable
         super.didReceiveMemoryWarning()
     }
     
+    var habits = [Habit]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            CoreDataHelper.delete(habit: habits[indexPath.row])
+            habits = CoreDataHelper.retrieveHabits()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "PointsCell") as! HabitsPointsCell
-//            cell.usernameLabel.text = post.poster.username
-            
-                return cell
-            
-        case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCell") as! AddHabitsCell
-//            let imageURL = URL(string: post.imageURL)
-//            cell.postImageView.kf.setImage(with: imageURL)
-            
-                return cell
-            
-        case 2:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "HabitCell") as! ListHabitsCell
-//            cell.delegate = self
-//            configureCell(cell, with: post)
-            
-                return cell
-            
-        default:
-            fatalError("Error: unexpected indexPath.")
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HabitCell") as! ListHabitsCell
+        
+        let row = indexPath.row
+        
+        let habit = habits[row]
+        
+        cell.nameOfHabit.text = habit.habit
+        
+        cell.numberOfDays.text = "\(habit.days) DAYS"
+        
+        return cell
+        
+//        switch indexPath.row {
+//        case 0:
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "PointsCell") as! HabitsPointsCell
+////            cell.usernameLabel.text = post.poster.username
+//            
+//                return cell
+//            
+//        case 1:
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCell") as! AddHabitsCell
+////            let imageURL = URL(string: post.imageURL)
+////            cell.postImageView.kf.setImage(with: imageURL)
+//            
+//                return cell
+//            
+//        case 2:
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "HabitCell") as! ListHabitsCell
+////            cell.delegate = self
+////            configureCell(cell, with: post)
+//            
+//                return cell
+//            
+//        default:
+//            fatalError("Error: unexpected indexPath.")
+//        }
 
-    } // commented section causes errors and this doesnt work idk :(
+    }
     
 //    func tableView(_ tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 //        
@@ -85,24 +110,24 @@ class ListHabitsViewController: UIViewController, UITableViewDataSource, UITable
         }
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if let identifier = segue.identifier {
-//            if identifier == "displayNote" {
-//                print("Table view cell tapped")
-//                
-////                let indexPath = tableView.indexPathForSelectedRow!
-//                
-////                let note = notes[indexPath.row]
-//                
-////                let displayNoteViewController = segue.destination as! DisplayNoteViewController
-//                
-////               displayNoteViewController.note = note
-//                
-//            } else if identifier == "addNote" {
-//                print("+ button tapped")
-//            }
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "displayHabit" {
+                print("Table view cell tapped")
+                
+                let indexPath = tableView.indexPathForSelectedRow!
+                
+                let habit = habits[indexPath.row]
+                
+                let habitInformationViewController = segue.destination as! HabitInformationViewController
+                
+               habitInformationViewController.habit = habit
+                
+            } else if identifier == "addHabit" {
+                print("+ button tapped")
+            }
+        }
+    }
 
 
 }
